22bd2c0d52b4eafd79766878266d59dee06513de
Irina16111982 <i.a.kaparushkina@tmn3.etagi.com>
2024-10-21 08:55:16 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+completed_homework=12
+zapyataya=','
+print(completed_homework)
+hours_spent=1.5
+print(hours_spent)
+name_cours='Python,'
+print(name_cours)
+time_for_one_task=0.1+0.025
+print(time_for_one_task)
+print('Курс:',name_cours,'всего задач:',completed_homework,zapyataya,'затрачено часов:',hours_spent,zapyataya,"среднее время выполнения:",time_for_one_task," часа.")
+#все
